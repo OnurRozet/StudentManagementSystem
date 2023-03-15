c2606a3ed9c1555e865353f751c2dd83a464b209
Onur <onurozet@hotmail.com>
2023-03-15 14:55:45 +0300

    Add project files.

---
+++ StudentManagementSystem/StudentContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem
+{
+	public static class StudentContext
+	{
+		public static List<Student> Students = new List<Student>
+		{
+			new Student{Id=1, Name="Onur",Surname="Rozet",Branch="A"},
+		    new Student{Id=2, Name="Ahmet Selçuk",Surname="Ozyurt",Branch="A"},
+		    new Student{Id=3, Name="Hasan Kemal",Surname="Itisken",Branch="B"},
+		    new Student{Id=4, Name="Ömer Faruk",Surname="Doğru",Branch="B"},
+		    new Student{Id=5, Name="AHmet Tolga",Surname="Odabaşı",Branch="C"},
+		};
+	}
+}
